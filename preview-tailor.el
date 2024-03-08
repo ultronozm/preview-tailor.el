4f279840344dcc4c6e774c03978642c7666cea8a
Paul Nelson <ultrono@gmail.com>
2024-03-08 22:30:25 +0100

    remove auctex dependency

--- preview-tailor.el
+++ preview-tailor.el
@@ -5,7 +5,7 @@
 ;; Author: Paul D. Nelson <nelson.paul.david@gmail.com>
 ;; Version: 0.0
 ;; URL: https://github.com/ultronozm/preview-tailor.el
-;; Package-Requires: ((emacs "29.1") (auctex "11.86.1"))
+;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: tex, multimedia
 
 ;; This program is free software; you can redistribute it and/or modify
